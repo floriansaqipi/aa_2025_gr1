1d5df257e0d54c7b42c217daa61ba13e457a3117
Gent Zhushi <132105731+gentzhushi@users.noreply.github.com>
2025-12-04 17:26:45 +0100

    ggez

--- assignment_9/zhushi/knapsack_problem.cpp
+++ assignment_9/zhushi/knapsack_problem.cpp
@@ -4,9 +4,10 @@ struct backpack_item {
 	int weight;
 	int value;
 	float value_per_weight = 0;
-}
+};
 
 int solve_knapsack(std::vector<backpack_item> INSTANCE, int BACKPACK_CAPACITY);
+void sort_by_coefficient(std::vector<backpack_item>& INSTANCE);
 
 int main(){
 
@@ -18,22 +19,50 @@ int main(){
 		backpack_item{1, 4},
 		backpack_item{1, 4},
 		backpack_item{1, 4},
-	}
+	};
 		
 	int backpack_capacity = 4;
 
-	int result = solve_knapsack(instance, backpack_capacity);
+	unsigned int result = solve_knapsack(instance, backpack_capacity);
 
-	std::cout << "Vlera më e lartë për instancën e përdorur është " << result << "!\n";
+	std::cout << "Sipas algoritmit, vlera më e lartë për instancën e përdorur është " << result << "!\n";
 
 	return 0;
 }
 
-int solve_knapsack(std::vector<backpack_item> INSTACE, int BACKPACK_CAPACITY){
+int solve_knapsack(std::vector<backpack_item> INSTANCE, int BACKPACK_CAPACITY){
 
 	// llogarite koeficientin val per wt
 	// sorto sipas qatij koeficienti
 	// edhe nisja, while theres capacity... += value; -= weight
 
-	return total
+	size_t n = INSTANCE.size();
+	for (int i = 0; i < n; i++) {
+		INSTANCE[i].value_per_weight = 1.0 * INSTANCE[i].value / INSTANCE[i].weight;
+	}
+	
+	sort_by_coefficient(INSTANCE);
+
+	int available_space = BACKPACK_CAPACITY;
+	int total_value= 0;
+	for (int i = 0; available_space > 0 && i < n; i++){
+		total_value += INSTANCE[i].value;
+		available_space -= INSTANCE[i].weight;
+	}
+
+	return total_value;
+}
+
+void sort_by_coefficient(std::vector<backpack_item>& INSTANCE){
+	
+	size_t n = INSTANCE.size();
+	for (int i = 0; i < n-1; i++){
+		for (int j = i; j < n; j++){
+			if (INSTANCE[i].value_per_weight > INSTANCE[j].value_per_weight){
+				backpack_item temp = INSTANCE[i];
+				INSTANCE[i] = INSTANCE[j];
+				INSTANCE[j] = temp;
+			}
+		}
+	}
 }
